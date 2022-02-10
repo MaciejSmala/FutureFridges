61109075adc45fed90df5d3cd09a90f51c5e2fa5
MaciejSmala <82884471+MaciejSmala@users.noreply.github.com>
2022-02-10 17:09:45 +0000

    Add files via upload

---
+++ src/fridges/Controller.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fridges;
+
+/**
+ *
+ * @author smala
+ */
+public class Controller implements IController {
+
+    private Model model;
+    private LoginView view;
+
+    public Controller(Model model) {
+        this.setModel(model);
+    }
+    @Override
+    public void addView(LoginView view){
+        this.view = view;
+    }
+
+    @Override
+    public Model getModel() {
+        return model;
+    }
+    @Override
+    public void setModel(Model model){
+        this.model = model;
+    }
+
+    @Override
+    public void loginButtonClicked() {
+        String text = view.userNameField.getText();
+                //model.setLogin(text);
+                if (view.checkdb().equals("Chef")) {
+                    View2.createAndShowGUI();
+                }
+                view.userNameField.setText("");
+                view.inputLabel.setText(text);
+    }
+}
---
+++ src/fridges/observer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fridges;
+
+/**
+ *
+ * @author smala
+ */
+public interface Observer {
+
+    public void loginChanged();
+    
+    public String checkdb();
+
+}
